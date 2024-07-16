334402e7dc409527631f549cfc28827256fbf371
unknown <stopkakrzysztof1@gmail.com>
2024-07-16 15:18:55 +0200

    Install framer-motion

--- rss-reader/src/components/FavoriteButton.jsx
+++ rss-reader/src/components/FavoriteButton.jsx
@@ -4,6 +4,7 @@ import { addFavorite, removeFavorite } from "../redux/slices/favoritesSlices";
 import { toggleFavorite } from "../redux/slices/articlesSlices";
 import { useDispatch } from "react-redux";
 import PropTypes from 'prop-types';
+import {motion} from 'framer-motion';
 
 export default function FavoriteButton ({article}){
     const dispatch = useDispatch();
@@ -21,9 +22,14 @@ export default function FavoriteButton ({article}){
     }
 
     return (
-        <div onClick={handleClick}
-        className='flex flex-row gap-1 items-center cursor-pointer'>
-            <p>{article.isFavorite ? <Heart size={16} variant="Bold" color="red"/> : <Heart size={16}/>}</p>
+        <div onClick={handleClick} className='flex flex-row gap-1 items-center cursor-pointer'>
+            <motion.div
+                whileHover={{ scale: 1.3 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            >
+            <p className="">{article.isFavorite ? <Heart size={16} variant="Bold" color="red"/> : <Heart size={16}/>}</p>
+            </motion.div>
         </div>
     )
 }
