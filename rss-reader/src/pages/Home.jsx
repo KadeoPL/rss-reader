8d6c52bb0330b5f0a0f50583019e7738279340df
unknown <stopkakrzysztof1@gmail.com>
2024-07-20 11:40:30 +0200

    Add searchParams to sort by category

--- rss-reader/src/pages/Home.jsx
+++ rss-reader/src/pages/Home.jsx
@@ -3,9 +3,10 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchArticles } from "../redux/slices/articlesSlices";
 import Article from "../components/Article";
 import Navigation from "../components/Navigation";
-import {CloseCircle} from 'iconsax-react';
+import { CloseCircle } from 'iconsax-react';
 import { motion } from 'framer-motion';
-
+import { useSearchParams } from "react-router-dom";
+import chooseCategoryColor from "../functions/categoryColor";
 export default function Home() {
   const dispatch = useDispatch();
   const articles = useSelector((state) => state.articles.items);
@@ -14,6 +15,7 @@ export default function Home() {
   const [hideRead, setHideRead] = useState(false);
   const [isSortByCategory, setIsSortByCategory] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState('all');
+  const [searchParams, setSearchParams] = useSearchParams();
   const [categoryColor, setCategoryColor] = useState('');
 
   useEffect(() => {
@@ -28,19 +30,29 @@ export default function Home() {
 
   const handleCategorySelection = (category) => {
     setSelectedCategory(category.category);
-    setCategoryColor(category.color);
-    setIsSortByCategory(true)
+    setCategoryColor(chooseCategoryColor(selectedCategory));
+    setSearchParams({ category: category.category});
+    setIsSortByCategory(true);
   };
+
+  useEffect(() => {
+    const category = searchParams.get("category");
+    if (category) {
+      setSelectedCategory(category);
+      setIsSortByCategory(true);
+    }
+  }, [searchParams]);
 
   let content;
   if (articlesStatus === 'loading') {
     content = <p>Loading...</p>;
   } else if (articlesStatus === 'succeeded') {
     const filteredArticles = articles
-    .filter(article => !hideRead || !article.isRead)
-    .filter(article => selectedCategory === 'all' || article.category === selectedCategory);
+      .filter(article => !hideRead || !article.isRead)
+      .filter(article => selectedCategory === 'all' || article.category === selectedCategory);
+
     content = filteredArticles.map(article => (
-      <Article article={article} key={article.id} onData={handleCategorySelection}/>
+      <Article article={article} key={article.id} onData={handleCategorySelection} />
     ));
   } else if (articlesStatus === 'failed') {
     content = <p>{error}</p>;
@@ -55,7 +67,20 @@ export default function Home() {
           <p>Hide read</p>
         </div>
         <div>
-          {isSortByCategory ? <motion.button whileHover={{scale: 1.1}} className={`${categoryColor} py-1 px-3 text-sm rounded-md text-white flex gap-2 items-center`} onClick={() => {setIsSortByCategory(false); setSelectedCategory('all')}}> <CloseCircle size={16}/>{selectedCategory}</motion.button> : <></>}
+          {isSortByCategory && (
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              className={`${categoryColor} py-1 px-3 text-sm rounded-md text-white flex gap-2 items-center`}
+              onClick={() => {
+                setIsSortByCategory(false);
+                setSelectedCategory('all');
+                setSearchParams({});
+              }}
+            >
+              <CloseCircle size={16} />
+              {selectedCategory}
+            </motion.button>
+          )}
         </div>
       </div>
       <div className="flex flex-row flex-wrap gap-x-6 gap-y-10 mx-5 justify-center">
